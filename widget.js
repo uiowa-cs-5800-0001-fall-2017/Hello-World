4d238141403362256f59b2db23b4df13c5511233
Sean Mullan <seanmm@mchsi.com>
2017-12-08 13:58:14 -0600

    Split css for separate chatbot styling

--- widget.js
+++ widget.js
@@ -1,92 +1,45 @@
-if(typeof Chatbot !== 'undefined'){}else{
+if (typeof Chatbot !== 'undefined') {} else {
   Chatbot = {};
 }
 Chatbot.getScriptFromServer = function(userId) {
-    // This is for end users since it doesn’t require logging in.
-    // Returns a promise, so use .then on the return variable
-    // ex: Chatbot.getBlocksFromServer(x,y).then(function(data){console.log(data);});
-    console.log(userId);
-    return $.get(`https://blocklychatbot.firebaseio.com/${userId}/script.json`).done(function(data) {
-      console.log(`GET succeeded`);
-      return data;
-    }).fail(function(error) {
-      console.log(`GET failed`);
-      console.log(error);
-    });
-   };
-$(document).ready(function(){
-   $('html > head').append(`
-     <style>
-     /* Chat stylings */
-     #chat-bubble-message{
-       height: 50px;
-       width: 85%;
-       background: white;
-       position: relative;
-       cursor: pointer;
-       border: 3px double black;
-       border-radius: 10px;
-       left: 13%;
-     }
-     #chat-bubble-message::before{
-       content: “”;
-       position: absolute;
-       top: 100%;
-       right: 30px;
-       height: 20px;
-       width: 20px;
-       background: white;
-       box-sizing:border-box;
-       transform: rotate(45deg) translate(-50%);
-       border-bottom: inherit;
-       border-right: inherit;
-       box-shadow: inherit;
-     }
+  // This is for end users since it doesn’t require logging in.
+  // Returns a promise, so use .then on the return variable
+  // ex: Chatbot.getBlocksFromServer(x,y).then(function(data){console.log(data);});
+  return $.get(`https://blocklychatbot.firebaseio.com/${userId}/script.json`).done(function(data) {
+    console.log(`GET succeeded`);
+    return data;
+  }).fail(function(error) {
+    console.log(`GET failed`);
+    console.log(error);
+  });
+};
 
-     #bot-bubble-message{
-       height: 50px;
-       width: 85%;
-       background: gray;
-       position: relative;
-       cursor: pointer;
-       border: 3px double black;
-       border-radius: 10px;
-     }
-     #bot-bubble-message::before{
-           content: “”;
-       position: absolute;
-       top: 100%;
-       left: 30px;
-       height: 20px;
-       width: 20px;
-       background: gray;
-       box-sizing:border-box;
-       transform: rotate(45deg) translate(-50%);
-       border-bottom: inherit;
-       border-right: inherit;
-       box-shadow: inherit;
-     }
-     </style>`);
-
-     $('#BlocklyWidget').append(`
-    <div id=“chat-container” style="
-             height: calc(100% - 3em);
-             width: 100%;
-             border-style: solid;
-             background-color: lightblue;
-             overflow-y: scroll;
-             word-wrap: break-word;
-             font-family: verdana;"></div>
-      <textarea id=“chat-input” style="
-             height: 3em;
-             width: 100%;
-             resize: none;
-             overflow-y: scroll;
-             border-style: solid;"
-             contenteditable=“true” spellcheck=“true”></textarea>`);
-   Chatbot.getScriptFromServer($("#BlocklyWidget").attr("data")).then(function(data){
-    eval(data);
-    console.log(data);
-  })
+$(function() {
+  var uid = $("#BlocklyChatbot").attr("data");
+  var link = document.createElement('link');
+  link.setAttribute("rel","stylesheet");
+  link.setAttribute("type", "text/css");
+  link.onload = function(){
+    var chatContainer = document.createElement('div');
+    chatContainer.setAttribute("id","chat-container");
+    var chatInput = document.createElement('textarea');
+    chatInput.setAttribute("id","chat-input");
+    chatInput.setAttribute("contenteditable","true");
+    chatInput.setAttribute("spellcheck","true");
+    chatInput.loadCode = function(){
+      Chatbot.getScriptFromServer(uid).then(function(data) {
+            try {
+              eval(data); //jshint ignore: line
+            } catch (e) {
+              alert(e); //jshint ignore: line
+            }
+      });
+    };
+    document.getElementById("BlocklyChatbot").appendChild(chatContainer);
+    document.getElementById("BlocklyChatbot").appendChild(chatInput);
+    chatInput.loadCode();
+  };
+  link.setAttribute("href", "chatbot.css");
+  document.getElementsByTagName("head")[0].appendChild(link);
 
 });
